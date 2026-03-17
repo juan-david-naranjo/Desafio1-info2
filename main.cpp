a8591e56f34cfd829096199cc4d84607de5c66de
juan <juan.naranjoj@udea.edu.co>
2026-03-16 23:16:50 -0500

    figuras I y 2x2 hechas

--- main.cpp
+++ main.cpp
@@ -10,7 +10,10 @@ void limpiarConsola();
 bool valido(unsigned int valor);    //valdiar dimemsiones tabla
 char updtable();      //actuaizar la tabla
 void generateform(unsigned int col,unsigned int fil,char**table,unsigned int &x,unsigned int &y);        // generar las fichas
-bool actficha(unsigned int &Xc, unsigned int &Yc, char**table,char &state,unsigned int &rot);        //actualiza el estado de la ficha
+void moveI(unsigned int &Xc, unsigned int &Yc, char**table,char &state,unsigned int &rot);        //actualiza el estado de la ficha
+void move2x2(unsigned int &Xc, unsigned int &Yc, char**table,char &state,unsigned int &rot);    //actualiza el estado de la ficha 2x2
+void moveL1(unsigned int &Xc, unsigned int &Yc, char**table,char &state,unsigned int &rot);     //actualiza el estado de la ficha L inversa
+void renderL(unsigned int Xc, unsigned int Yc, char** table, unsigned int rot); //dibuja a la pieza L
 
 
 int main()
@@ -19,7 +22,7 @@ int main()
     unsigned int col,fil;
     bool exit=true;
     char name[20];
-
+    unsigned short int control=0;
     unsigned int rotacion=0;
     // codigo principal del juego
     while(exit){
@@ -29,62 +32,80 @@ int main()
 
         cout<<"Ingresa tu nombre de jugador: "<<endl;
         cin>>name;
-        cout<<"Ingresa las Dimensiones del tetrtis (multiplos de 8)"<<endl;
+        cin.ignore(100, '\n');
+        cout<<"1.Nueva partida"<<endl;
+        cout<<"2.Salir"<<endl;
+        cin>>control;
+        switch (control) {
+        case 1:
+            cout<<"Bienvenido: "<<name<<endl;
+            cout<<"Ingresa las Dimensiones del tetrtis (multiplos de 8)"<<endl;
 
 
 
-        //validar dimensiones sean multiplos de 8
-        cout<<"Ingresa la cantidad de columnas: ";
-        cin>>col;
-        cout<<"Ingresa la cantidad de filas: ";
-        cin>>fil;
+            //validar dimensiones sean multiplos de 8
+            cout<<"Ingresa la cantidad de columnas: ";
+            cin>>col;
+            cout<<"Ingresa la cantidad de filas: ";
+            cin>>fil;
 
 
-        //reserva de las dimensiones del tablero
-        if(valido(col)&&valido(fil)){
-            char **table = new char*[col];
-            for(unsigned int i=0;i<col;i++){
-                table[i]=new char[fil];
-                for (unsigned int j=0;j<fil;j++){
-                    table[i][j]=0;
+            //reserva de las dimensiones del tablero
+            if(valido(col)&&valido(fil)){
+                col+=2;
+                char **table = new char*[col];
+                for(unsigned int i=0;i<col;i++){
+                    table[i]=new char[fil];
+                    for (unsigned int j=0;j<fil;j++){
+                        table[i][j]=0;
+                    }
                 }
+                unsigned int X=0;
+                unsigned int Y=0;
+
+                printtable(col,fil,table);
+                generateform(col,fil,table,X,Y);
+
+
+
+
+
+
+
+
+
+
+                //eliminacion del tablero dinamico
+                for(unsigned int i=0;i<col;i++){
+                    delete []table[i];
+                }
+                delete []table;
+                cout<<"tabla eliminada"<<endl;
+            }else{
+                cout<<"Recuerde que las dimensiones del tetris deben ser multiplos de 8"<<endl;
             }
-            unsigned int X=0;
-            unsigned int Y=0;
-
-            printtable(col,fil,table);
-            generateform(col,fil,table,X,Y);
-
-
-
-        //eliminacion del tablero dinamico
-            for(unsigned int i=0;i<col;i++){
-                delete []table[i];
-            }
-            delete []table;
-            cout<<"tabla eliminada"<<endl;
 
 
 
 
-        }else{
-            cout<<"Recuerde que las dimensiones del tetris deben ser multiplos de 8"<<endl;
+
+
+
+            break;
+
+        case 2:
+            cout<<"Saliendo del juego"<<endl;
             exit=false;
+            break;
+        default:
+            cout<<"Ingrese una opcion valida"<<endl;
+            break;
         }
-        exit=false;
-
-
-
-
 
 
 
 
     }
-
-
-
-
 
 }
 
@@ -92,7 +113,7 @@ int main()
 // funciones del tetris
 void printtable(unsigned int col,unsigned int fil,char **table){
     limpiarConsola();
-    for (int i = 0; i < col; i++) {
+    for (int i = 2; i < col; i++) {
         for (int j = 0; j < fil; j++) {
 
             switch(table[i][j]) {
@@ -152,15 +173,15 @@ char updtable(){
         return 'a';
     case 'd':
     case 'D':
-        cout<<"Ingresaste W"<<endl;
+        cout<<"Moviendose a la Der"<<endl;
        return 'd';
     case 's':
     case 'S':
-        cout<<"Ingresaste W"<<endl;
+        cout<<"Bajando un bit"<<endl;
         return 's';
     case 'w':
     case'W':
-        cout<<"Ingresaste W"<<endl;
+        cout<<"Rotando 90 grados"<<endl;
         return 'w';
     case 'q':
     case 'Q':
@@ -177,39 +198,59 @@ char updtable(){
 
 // generar las fichas
 void generateform(unsigned int col,unsigned int fil,char**table,unsigned int &x,unsigned int &y){
-    unsigned short int form=0;
+    unsigned short int form=2;
     char state;
     unsigned int rot=0;
     unsigned int mid=(fil/2)-1;
-    x=0;
+    x=1;
     y=mid;
+
+    int c=0;
     switch (form) {
     case 0:
+        table[x][mid]=1;
         cout<<"pieza I"<<endl;  //al inicio las coordenadas de la ficha es la mitad
+        table[x+1][y]^=1;
+        while(c<16){
 
-        for(unsigned short int i=0;i<3;i++){
+            printtable(col,fil,table);
             state=updtable();
-            if(actficha(x,y,table,state,rot)){
-                table[x][y]=1;
-                printtable(col,fil,table);}
+            moveI(x,y,table,state,rot);
+             cout<<"saliendo"<<endl;
+             c++;
         }
+
+
+
+
 
         break;
     case 1:
-        cout<<"pieza 2x2"<<endl;
-        for(unsigned short int i=0;i<2;i++){
-            table[i][mid]=1;
-            table[i][fil+1]=1;
+        table[x][mid]=1;
+        table[x-1][y]^=1;
+        table[x-1][y+1]^=1;     //inicializamos el cuadro 2x2
+        table[x][y+1]^=1;
+        while(c<16){
+
             printtable(col,fil,table);
+            state=updtable();
+            move2x2(x,y,table,state,rot);
+            cout<<"saliendo"<<endl;
+            c++;
         }
         break;
 
     case 2:
-        cout<<"pieza L inversa"<<endl;
-        for(unsigned short int i=0;i<3;i++){
-            table[i][fil]=1;
-            if(i==0){table[i][fil-1]=1;}
+        // table[x-1][y]^=1;
+        // table[x-1][y+1]^=1;     //inicializamos L
+        // table[x+1][y]^=1;
+        while(c<16){
+
             printtable(col,fil,table);
+            state=updtable();
+            moveL1(x,y,table,state,rot);
+            cout<<"saliendo"<<endl;
+            c++;
         }
         break;
 
@@ -242,7 +283,7 @@ void generateform(unsigned int col,unsigned int fil,char**table,unsigned int &x,
     case 6:
         cout<<"pieza z  "<<endl;
         for(unsigned short int i=0;i<2;i++){
-            table[i][11];
+            table[i][1];
             if(i==0){table[i][+1]=1;}else{table[i][-1]=1;}
 
             printtable(col,fil,table);
@@ -257,35 +298,180 @@ void generateform(unsigned int col,unsigned int fil,char**table,unsigned int &x,
 }
 
 
-bool actficha(unsigned int &Xc, unsigned int &Yc, char**table,char &state,unsigned int &rot){
+void moveI(unsigned int &Xc, unsigned int &Yc, char**table,char &state,unsigned int &rot){
     //Xcentro es la coordenada central en x de la ficha<--->Ycentro es la coordenada central en y de la ficha
     switch(state) {
     case 'a':
         Yc-=1;
-
-        return true;
+        table[Xc-1][Yc]^=1;
+        table[Xc][Yc]^=1;
+        table[Xc+1][Yc]^=1;
+        // limpiar movimiento
+        table[Xc-1][Yc+1]&=0;
+        table[Xc][Yc+1]&=0;
+        table[Xc+1][Yc+1]&=0;
+        return;
     case 's':
         Xc+=1;
+        table[Xc-1][Yc]&=1;
+        table[Xc][Yc]&=1;
+        table[Xc+1][Yc]^=1;
+        // limpiar movimiento
+        table[Xc-2][Yc]&=0;
 
-        return true;
+
+        return;
     case 'd':
-        Xc+=1;
+        Yc+=1;
+        table[Xc-1][Yc]^=1;
+        table[Xc][Yc]^=1;
+        table[Xc+1][Yc]^=1;
+        // limpiar movimiento
+        table[Xc-1][Yc-1]&=0;
+        table[Xc][Yc-1]&=0;
+        table[Xc+1][Yc-1]&=0;
 
-        return true;
+        return;
     case 'w':
-        rot+=1;
+        // 1. Borrar la forma actual ANTES de cambiar la rotación
+        if (rot % 2 == 0) { // Estaba vertical
+            table[Xc-1][Yc] ^= 1;
+            table[Xc][Yc] &= 1;
+            table[Xc+1][Yc] ^= 1;
+        } else { // horizontal
+            table[Xc][Yc-1] &= 0;
+            table[Xc][Yc] &= 1;
+            table[Xc][Yc+1] &= 0;
+        }
 
-        return true;
+        // 2. Cambiar el estado de rotación
+        rot = (rot + 1) % 2; // Alterna entre 0 y 1
+
+        // 3. Dibujar la nueva forma
+        if (rot != 0) { // Ahora es Horizontal
+            table[Xc][Yc-1] ^= 1;
+            table[Xc][Yc] &= 1;
+            table[Xc][Yc+1] ^= 1;
+        } else { // Ahora es Vertical
+            table[Xc-1][Yc] ^= 1;
+            table[Xc][Yc] &= 1;
+            table[Xc+1][Yc] ^= 1;
+        }
+
+
+        return;
     case 'q':
-
-        return false;
+        cout<<"caso q"<<endl;
+        return;
     default:
-        return false;
+        return;
     }
 
+}
 
 
+void move2x2(unsigned int &Xc, unsigned int &Yc, char**table,char &state,unsigned int &rot){
+    //Xcentro es la coordenada central en x de la ficha<--->Ycentro es la coordenada central en y de la ficha
+    //inicializar la ficha
 
+    switch(state) {
+    case 'a':
+        Yc-=1;
+        table[Xc-1][Yc]^=1;
+        table[Xc][Yc]^=1;
+        // limpiar movimiento
+        table[Xc-1][Yc+2]&=0;
+        table[Xc][Yc+2]&=0;
+        return;
+    case 's':
+        Xc+=1;
+        table[Xc-1][Yc]&=1;
+        table[Xc-1][Yc+1]&=1;
+        table[Xc][Yc]^=1;
+        table[Xc][Yc+1]^=1;
+        // limpiar movimiento
+        table[Xc-2][Yc]&=0;
+        table[Xc-2][Yc+1]&=0;
+
+
+        return;
+    case 'd':
+        Yc+=1;
+        table[Xc-1][Yc+1]^=1;
+        table[Xc][Yc+1]^=1;
+        // limpiar movimiento
+        table[Xc-1][Yc-1]&=0;
+        table[Xc][Yc-1]&=0;
+
+        return;
+    case 'w':
+        //2x2 no rota
+        return;
+    case 'q':
+        cout<<"caso q"<<endl;
+        return;
+    default:
+        return;
+    }
 
 }
+
+void moveL1(unsigned int &Xc, unsigned int &Yc, char** table, char &state, unsigned int &rot) {
+    // 1. Borrar la posición actual (XOR invierte el bit de 1 a 0)
+    renderL(Xc, Yc, table, rot);
+
+    // 2. Actualizar según la tecla
+    switch(state) {
+    case 'a': // Izquierda
+        Yc -= 1;
+        break;
+    case 'd': // Derecha
+        Yc += 1;
+        break;
+    case 's': // Abajo
+        Xc += 1;
+        break;
+    case 'w': // Rotar 90 grados
+        rot = (rot + 1) % 4;
+        break;
+    case 'q':
+        return;
+    }
+
+    // 3. Pintar en la nueva posición (XOR invierte el bit de 0 a 1)
+    renderL(Xc, Yc, table, rot);
+}
+
+
+void renderL(unsigned int Xc, unsigned int Yc, char** table, unsigned int rot) {
+    // El centro siempre es parte de la ficha
+    table[Xc][Yc] ^= 1;
+
+    switch(rot % 4) {
+    case 0: // 0° - L normal parada
+        table[Xc-2][Yc] ^= 1;
+        table[Xc-1][Yc] ^= 1;
+        table[Xc+1][Yc] ^= 1;
+        table[Xc+1][Yc+1] ^= 1;
+        break;
+    case 1: // 90° - L acostada
+        table[Xc][Yc+1] ^= 1;
+        table[Xc][Yc-1] ^= 1;
+        table[Xc+1][Yc-1] ^= 1;
+
+        break;
+    case 2: // 180° - L cabeza abajo
+        table[Xc+1][Yc] ^= 1;
+        table[Xc-1][Yc] ^= 1;
+        table[Xc-1][Yc-1] ^= 1;
+        break;
+    case 3: // 270° - L acostada invertida
+        table[Xc][Yc-1] ^= 1;
+        table[Xc][Yc+1] ^= 1;
+        table[Xc-1][Yc+1] ^= 1;
+        break;
+    }
+}
+
+
 
